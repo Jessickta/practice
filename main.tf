b2d413bec5d8628ded229f969f2efece634ff0a2
jessickta <jt-ashley@hotmail.co.uk>
2018-08-13 13:06:35 +0100

    Adding practice tf project

---
+++ main.tf
@@ -0,0 +1,48 @@
+provider "aws" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+provider "archive" {}
+
+data "archive_file" "zip" {
+  type = "zip"
+  source_file = "hello_lambda.py"
+  output_path = "hello_lambda.zip"
+}
+
+resource "aws_iam_role" "lambda_iam_role" {
+  name = "lambda_iam_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = "hello_lambda"
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python2.7"
+
+  role = "${aws_iam_role.lambda_iam_role.arn}"
+  filename = "${data.archive_file.zip.output_path}"
+  source_code_hash = "${data.archive_file.zip.output_sha}"
+
+  environment {
+    variables = {
+      greeting = "Hello"
+    }
+  }
+}
---
+++ secret.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIAI4E6N72PFJKGPVRA"
+secret_key = "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"
