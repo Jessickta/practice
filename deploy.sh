a5a210d2585afc47eb0f422e3c2fc38120c3ad57
jessickta <jt-ashley@hotmail.co.uk>
2018-08-14 15:29:27 +0100

    Adding packaging

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-echo "Hello World"
+#Package endpoints lambda releases
+cd endpoints
+sudo apt-get install nodejs
+pwd
+for folderName in `find . -mindepth 1 -maxdepth 1 -type d`; do
+  echo $folderName
+  cd $folderName
+  npm install
+  zip -r lambda_function_payload.zip .
+  cd ..
+done;
 
+#Package SQS triggers lambda releases
+pwd
+cd sqs_triggers
+for folderName in `find . -mindepth 1 -maxdepth 1 -type d`; do
+  echo $folderName
+  cd $folderName
+  npm install
+  zip -r lambda_function_payload.zip .
+  cd ..
+done;
+
+#Apply terraform changes
+cd infrastructure
 terraform init
 terraform plan
