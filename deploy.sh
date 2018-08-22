a13e283a9395b7938ad7003aa15e7b66f5da795d
jessickta <jt-ashley@hotmail.co.uk>
2018-08-22 12:31:54 +0100

    Attempt to manipulate branch name

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+if [[ "${TRAVIS_BRANCH}" == "feature"* ]];
+then
+  echo "Feature branch yay";
+else
+  echo "Not feature boo";
+fi
+
 #Apply terraform changes
-terraform init
-terraform plan
+#terraform init
+#terraform plan
+
+#feature/jta/travis_script
+#develop/jta/whatevs
+#master
+#release/jta/whatevs
