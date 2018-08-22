fa9a7e28ed328b3ec21282a3e739a5f82998ab9c
jessickta <jt-ashley@hotmail.co.uk>
2018-08-22 14:16:50 +0100

    Adding elif statements

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-if [[ "${TRAVIS_BRANCH}" == "feature"* ]];
+if [[ "${TRAVIS_BRANCH}" == "feature"* ]]
 then
-  echo "Feature branch yay";
-else
-  echo "Not feature boo";
+  echo "Feature branch yay"
+  branch = "feature"
+elif [[ "$TRAVIS_BRANCH" == "develop"* ]]
+then
+  echo "Non-feature boo"
+  branch = "non-feat"
+elif [[ "$TRAVIS_BRANCH}" == "master" ]];
+then
+  echo "Master branch"
+  branch = "master"
 fi
+
 
 #Apply terraform changes
 #terraform init
